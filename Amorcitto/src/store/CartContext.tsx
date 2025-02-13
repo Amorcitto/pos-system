ac783ee41d321c59fca78d58c1515c6a7683e909
Jabrahamjohn <jabrahamjohns@gmail.com>
2025-02-14 01:03:59 +0300

    add react-router-dom for routing functionality; create CartContext for state management

---
+++ Amorcitto/src/store/CartContext.tsx
@@ -0,0 +1,36 @@
+import React, { createContext, useState, ReactNode } from 'react';
+
+interface CartItem {
+  id: number;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+interface CartContextType {
+  cart: CartItem[];
+  addToCart: (item: CartItem) => void;
+  removeFromCart: (id: number) => void;
+}
+
+const CartContext = createContext<CartContextType | undefined>(undefined);
+
+const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [cart, setCart] = useState<CartItem[]>([]);
+
+  const addToCart = (item: CartItem) => {
+    setCart((prevCart) => [...prevCart, item]);
+  };
+
+  const removeFromCart = (id: number) => {
+    setCart((prevCart) => prevCart.filter(item => item.id !== id));
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export { CartContext, CartProvider };
